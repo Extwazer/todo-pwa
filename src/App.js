ad8d98accd3d4018b3bd44bc23cff52924023a22
Stas Mischenko <extwazer@ukr.net>
2021-10-08 00:20:35 +0300

    done this shit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = JSON.parse(localStorage.getItem("to-do-list")) || {
+      elements: [],
+    };
+  }
+
+  componentDidUpdate = () => {
+    const { elements } = this.state;
+    localStorage.setItem("to-do-list", JSON.stringify({ elements }));
+    console.log("1");
+  };
+
+  addNewElement = (e) => {
+    if (e.keyCode === 13) {
+      this.setState({
+        elements: this.state.elements.concat({
+          id: Date.now(),
+          value: e.target.value,
+          isCheck: false,
+        }),
+      });
+    }
+  };
+
+  deleteElement = (elementId) => {
+    const { elements } = this.state;
+    const newArray = elements.filter(({ id }) => id !== elementId);
+    this.setState({
+      elements: newArray,
+    });
+  };
+
+  checkElement = (checkedId) => {
+    const { elements } = this.state;
+    const newArray = elements.map(({ id, value, isCheck }) => {
+      if (id === checkedId) {
+        isCheck = !isCheck;
+      }
+      return { id: id, value: value, isCheck };
+    });
+
+    this.setState({
+      elements: newArray,
+    });
+  };
+
+  checkAllElements = () => {
+    const { elements } = this.state;
+    let check = false;
+
+    elements.forEach((el) => {
+      if (!el.isCheck) {
+        check = true;
+      }
+    });
+
+    const newArray = elements.map(({ id, value, isCheck }) => {
+      return { id: id, value: value, isCheck: check };
+    });
+
+    this.setState({
+      elements: newArray,
+    });
+  };
+
+  removeCheckedElements = () => {
+    const { elements } = this.state;
+
+    const filteredArray = elements.filter(({ isCheck }) => !isCheck);
+
+    this.setState({
+      elements: filteredArray,
+    });
+  };
+
+  countFinishedElements = () => {
+    const { elements } = this.state;
+    const count = elements.filter(({ isCheck }) => isCheck !== false).length;
+    return count;
+  };
+
+  render() {
+    const { elements } = this.state;
+    return (
+      <div className="container margins">
+        <div className="col-8">
+          <div className="form-element">
+            <input type="text" onKeyDown={this.addNewElement}></input>
+          </div>
+          <ul className="form-lists">
+            {elements.map(({ id, value, isCheck }) => (
+              <li id={id} className={isCheck ? "light-green" : ""}>
+                <div className="element">{value}</div>
+                <div className="elements-btns">
+                  <button
+                    className="button checked"
+                    onClick={() => this.checkElement(id)}
+                  >
+                    {isCheck ? "Checked" : "Not Checked"}
+                  </button>
+
+                  <button
+                    className="button delete"
+                    onClick={() => this.deleteElement(id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-4">
+          <div className="sidebar">
+            <h2>Task Finished: {this.countFinishedElements()}</h2>
+            {elements.length ? (
+              <button
+                className="button"
+                onClick={() => this.checkAllElements()}
+              >
+                Check All
+              </button>
+            ) : (
+              ""
+            )}
+            {elements.length ? (
+              <button
+                className="button"
+                onClick={() => this.removeCheckedElements()}
+              >
+                Remove Checked
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
